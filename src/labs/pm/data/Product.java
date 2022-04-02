973d7d865d233a7b5560eb82dacd652069b50ccd
Arjun Menon <arjun.menon@oracle.com>
2022-04-02 21:33:58 +0530

    custom constructors and enums

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -3,37 +3,65 @@ package labs.pm.data;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import static labs.pm.data.Rating.*;
+
+
 public class Product {
-    private int id;
-    private String name;
-    private BigDecimal price;
+
+    final private int id;
+    final private String name;
+    final private BigDecimal price;
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private final Rating rating;
+
+    public Product() {
+        this(0, "", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+//    public void setPrice(BigDecimal price) {
+//        this.price = price;
+//    }
 
     public BigDecimal getDiscount() {
         return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public Product applyRating(Rating newRating) {
+        return new Product(id, name, price, newRating);
     }
 }
