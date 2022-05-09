1f2796ceaf457af380e010c3066500b120d96e00
Arjun Menon <arjunmenon@Arjuns-MacBook-Pro.local>
2022-05-09 20:39:23 +0530

    converting Product into sealed class

--- sources/labs/pm/data/Product.java
+++ sources/labs/pm/data/Product.java
@@ -1,98 +1,103 @@
 package labs.pm.data;
 
+import static labs.pm.data.Rating.NOT_RATED;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.Objects;
 
-import static labs.pm.data.Rating.NOT_RATED;
 
+public sealed abstract class Product implements Rateable<Product> permits Food, Drink {
 
-public abstract class Product implements Rateable<Product> {
-
-    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
-    final private int id;
-    final private String name;
-    final private BigDecimal price;
-    private final Rating rating;
+  public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+  final private int id;
+  final private String name;
+  final private BigDecimal price;
+  private final Rating rating;
 
 //     Product() {
 //        this(0, "", BigDecimal.ZERO);
 //    }
 
-    Product(int id, String name, BigDecimal price, Rating rating) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.rating = rating;
-    }
+  Product(int id, String name, BigDecimal price, Rating rating) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.rating = rating;
+  }
 
-    Product(int id, String name, BigDecimal price) {
-        this(id, name, price, NOT_RATED);
-    }
+  Product(int id, String name, BigDecimal price) {
+    this(id, name, price, NOT_RATED);
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
 //    public void setId(int id) {
 //        this.id = id;
 //    }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
 //    public void setName(String name) {
 //        this.name = name;
 //    }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
+  public BigDecimal getPrice() {
+    return price;
+  }
 
 //    public void setPrice(BigDecimal price) {
 //        this.price = price;
 //    }
 
-    public BigDecimal getDiscount() {
-        return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
-    }
+  public BigDecimal getDiscount() {
+    return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+  }
 
-    @Override
-    public Rating getRating() {
-        return rating;
-    }
+  @Override
+  public Rating getRating() {
+    return rating;
+  }
 
-    public LocalDate getBestBefore() {
-        return LocalDate.now();
-    }
+  public LocalDate getBestBefore() {
+    return LocalDate.now();
+  }
 
-    @Override
-    public String toString() {
-        return
-                "id=" + id +
-                        ", name='" + name + '\'' +
-                        ", price=" + price +
-                        ", discount=" + getDiscount() +
-                        ", rating=" + getRating() +
-                        ", bestBefore=" + getBestBefore();
-    }
+  @Override
+  public String toString() {
+    return
+        "id=" + id +
+            ", name='" + name + '\'' +
+            ", price=" + price +
+            ", discount=" + getDiscount() +
+            ", rating=" + getRating() +
+            ", bestBefore=" + getBestBefore();
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Product product = (Product) o;
-        return id == product.id && Objects.equals(name, product.name);
-    }
+  @Override
+  public boolean equals(Object o) {
+// old way of checking object equality
+//    if (this == o) {
+//      return true;
+//    }
+//    if (o == null || getClass() != o.getClass()) {
+//      return false;
+//    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
+    // using instanceof before casting o to Product does not cause a runtime exception in case o is cannot be cast to Product
+    if (o instanceof Product product) {
+      return id == product.id && Objects.equals(name, product.name);
     }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
