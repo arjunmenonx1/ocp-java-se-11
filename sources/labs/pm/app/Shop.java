9f89626c86326b5c2fac7fe8c762586c78c8232c
Arjun Menon <arjunmenon@Arjuns-MacBook-Pro.local>
2022-06-07 19:53:11 +0530

    lambda functions and nested classes

--- sources/labs/pm/app/Shop.java
+++ sources/labs/pm/app/Shop.java
@@ -2,7 +2,7 @@ package labs.pm.app;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Locale;
+import java.util.Comparator;
 import labs.pm.data.Product;
 import labs.pm.data.ProductManager;
 import labs.pm.data.Rating;
@@ -10,45 +10,53 @@ import labs.pm.data.Rating;
 public class Shop {
 
   public static void main(String[] args) {
-    ProductManager pm = new ProductManager(Locale.UK);
-    Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-    p1 = pm.reviewProduct(101, Rating.FOUR_STAR, "Nice hot cup of tea");
-    p1 = pm.reviewProduct(101, Rating.THREE_STAR, "Ok cup of tea");
-    p1 = pm.reviewProduct(101, Rating.FIVE_STAR, "Great cup of tea");
-    p1 = pm.reviewProduct(101, Rating.ONE_STAR, "Horrible cup of tea");
-    p1 = pm.reviewProduct(101, Rating.FOUR_STAR, "Nice hot cup of tea");
-    pm.printProductReport(101);
-    Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-    p2 = pm.reviewProduct(102, Rating.FOUR_STAR, "Nice hot cup of coffee");
-    p2 = pm.reviewProduct(102, Rating.THREE_STAR, "Ok cup of coffee");
-    p2 = pm.reviewProduct(102, Rating.THREE_STAR, "Nice coffee, mate!");
-    p2 = pm.reviewProduct(102, Rating.ONE_STAR, "Horrible cup of coffee");
-    p2 = pm.reviewProduct(102, Rating.ONE_STAR, "I asked for tea");
-    pm.printProductReport(102);
-    Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR,
+    ProductManager pm = new ProductManager("en-GB");
+    pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
+    pm.reviewProduct(101, Rating.FOUR_STAR, "Nice hot cup of tea");
+    pm.reviewProduct(101, Rating.THREE_STAR, "Ok cup of tea");
+    pm.reviewProduct(101, Rating.FIVE_STAR, "Great cup of tea");
+    pm.reviewProduct(101, Rating.ONE_STAR, "Horrible cup of tea");
+    pm.reviewProduct(101, Rating.FOUR_STAR, "Nice hot cup of tea");
+//    pm.printProductReport(101);
+    pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+    pm.reviewProduct(102, Rating.FOUR_STAR, "Nice hot cup of coffee");
+    pm.reviewProduct(102, Rating.THREE_STAR, "Ok cup of coffee");
+    pm.reviewProduct(102, Rating.THREE_STAR, "Nice coffee, mate!");
+    pm.reviewProduct(102, Rating.ONE_STAR, "Horrible cup of coffee");
+    pm.reviewProduct(102, Rating.ONE_STAR, "I asked for tea");
+//    pm.printProductReport(102);
+    pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR,
         LocalDate.now().plusDays(2));
-    p3 = pm.reviewProduct(103, Rating.FOUR_STAR, "Cake Cake Cake!");
-    p3 = pm.reviewProduct(103, Rating.THREE_STAR, "Just fine");
-    p3 = pm.reviewProduct(103, Rating.THREE_STAR, "Needs more cream");
-    p3 = pm.reviewProduct(103, Rating.ONE_STAR, "Not good at all");
-    p3 = pm.reviewProduct(103, Rating.ONE_STAR, "I asked for tea again");
-    pm.printProductReport(103);
-    Product p4 = pm.createProduct(104, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR,
+    pm.reviewProduct(103, Rating.FOUR_STAR, "Cake Cake Cake!");
+    pm.reviewProduct(103, Rating.THREE_STAR, "Just fine");
+    pm.reviewProduct(103, Rating.THREE_STAR, "Needs more cream");
+    pm.reviewProduct(103, Rating.ONE_STAR, "Not good at all");
+    pm.reviewProduct(103, Rating.ONE_STAR, "I asked for tea again");
+//    pm.printProductReport(103);
+    pm.createProduct(104, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR,
         LocalDate.now());
-    p4 = pm.reviewProduct(104, Rating.FOUR_STAR, "Nice and fresh");
-    p4 = pm.reviewProduct(104, Rating.FIVE_STAR, "Perfect");
-    pm.printProductReport(104);
-    Product p5 = pm.createProduct(105, "Hot Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
-    p5 = pm.reviewProduct(105, Rating.FOUR_STAR, "Delicious");
-    p5 = pm.reviewProduct(105, Rating.FIVE_STAR, "Amazing");
-    p5 = pm.reviewProduct(105, Rating.FIVE_STAR, "Made my day!");
-    p5 = pm.reviewProduct(105, Rating.THREE_STAR, "Portion size is too small");
-    pm.printProductReport(105);
-    Product p6 = pm.createProduct(106, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
-    p6 = pm.reviewProduct(106, Rating.FOUR_STAR, "My favorite");
-    p6 = pm.reviewProduct(106, Rating.THREE_STAR, "Sweet and decadent");
-    pm.printProductReport(106);
+    pm.reviewProduct(104, Rating.FOUR_STAR, "Nice and fresh");
+    pm.reviewProduct(104, Rating.FIVE_STAR, "Perfect");
+//    pm.printProductReport(104);
+//    pm.changeLocale("es-US");
+    pm.createProduct(105, "Hot Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
+    pm.reviewProduct(105, Rating.FOUR_STAR, "Delicious");
+    pm.reviewProduct(105, Rating.FIVE_STAR, "Amazing");
+    pm.reviewProduct(105, Rating.FIVE_STAR, "Made my day!");
+    pm.reviewProduct(105, Rating.THREE_STAR, "Portion size is too small");
+//    pm.printProductReport(105);
+    pm.createProduct(106, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
+    pm.reviewProduct(106, Rating.FOUR_STAR, "My favorite");
+    pm.reviewProduct(106, Rating.THREE_STAR, "Sweet and decadent");
+//    pm.printProductReport(106);
+    pm.printProducts((p1, p2) -> p2.getRating().ordinal() - p1.getRating()
+        .ordinal()); // lambda functions to sort based on rating
+    pm.printProducts((p1, p2) -> p2.getPrice().compareTo(p1.getPrice()));
 
+    Comparator<Product> ratingSorter = (p1, p2) -> p2.getRating().ordinal() - p1.getRating()
+        .ordinal();
+    Comparator<Product> priceSorter = (p1, p2) -> p2.getPrice().compareTo(p1.getPrice());
+    pm.printProducts(ratingSorter.thenComparing(priceSorter)); // Chaining comparators/sorters
   }
 
 }
--- sources/labs/pm/data/ProductManager.java
+++ sources/labs/pm/data/ProductManager.java
@@ -9,28 +9,43 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class ProductManager {
 
-  private final Locale locale;
-  private final ResourceBundle resources;
-  private final DateTimeFormatter dateFormat;
-  private final NumberFormat moneyFormat;
 
   private Product product;
   private Map<Product, List<Review>> products = new HashMap<>();
+  private ResourceFormatter formatter;
 
+  private static Map<String, ResourceFormatter> formatters = Map.of("en-GB",
+      new ResourceFormatter(Locale.UK),
+      "en-US", new ResourceFormatter(Locale.US),
+      "es-US", new ResourceFormatter(new Locale("es", "US")),
+      "fr-FR", new ResourceFormatter(Locale.FRANCE),
+      "zh-CN", new ResourceFormatter(Locale.CHINA)
+  );
+
+  public void changeLocale(String languageTag) {
+    formatter = formatters.getOrDefault(languageTag, formatters.get("en-GB"));
+  }
+
+  public static Set<String> getSupportedLocales() {
+    return formatters.keySet();
+  }
+
+  public ProductManager(String languageTag) {
+    changeLocale(languageTag);
+  }
 
   public ProductManager(Locale locale) {
-    this.locale = locale;
-    resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
-    dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
-    moneyFormat = NumberFormat.getCurrencyInstance(locale);
+    this(locale.toLanguageTag());
   }
 
   public Product createProduct(int id, String name, BigDecimal price, Rating rating,
@@ -64,21 +79,16 @@ public class ProductManager {
     List<Review> reviews = products.get(product);
     Collections.sort(reviews);
     StringBuilder txt = new StringBuilder();
-    txt.append(MessageFormat.format(resources.getString("product"),
-        product.getName(), moneyFormat.format(product.getPrice()),
-        product.getRating().getStars(),
-        dateFormat.format(product.getBestBefore())));
+    txt.append(formatter.formatProduct(product));
     txt.append('\n');
 
     for (Review review : reviews) {
-      txt.append(MessageFormat.format(resources.getString("review"),
-          review.getRating().getStars(),
-          review.getComments()));
+      txt.append(formatter.formatReview(review));
       txt.append('\n');
     }
 
     if (reviews.isEmpty()) {
-      txt.append(resources.getString("no.reviews"));
+      txt.append(formatter.getText("no.reviews"));
       txt.append('\n');
     }
     System.out.println(txt);
@@ -86,6 +96,18 @@ public class ProductManager {
 
   public void printProductReport(int id) {
     printProductReport(findProduct(id));
+  }
+
+  public void printProducts(Comparator<Product> sorter) {
+    List<Product> productList = new ArrayList<>(products.keySet());
+    productList.sort(sorter);
+    StringBuilder txt = new StringBuilder();
+
+    for (Product product : productList) {
+      txt.append(formatter.formatProduct(product));
+      txt.append('\n');
+    }
+    System.out.println(txt);
   }
 
   public Product findProduct(int id) {
@@ -101,6 +123,37 @@ public class ProductManager {
 
   public Product reviewProduct(int id, Rating rating, String comments) {
     return reviewProduct(findProduct(id), rating, comments);
+  }
+
+
+  private static class ResourceFormatter {
+
+    private final Locale locale;
+    private final ResourceBundle resources;
+    private final DateTimeFormatter dateFormat;
+    private final NumberFormat moneyFormat;
+
+    private ResourceFormatter(Locale locale) {
+      this.locale = locale;
+      resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
+      dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
+      moneyFormat = NumberFormat.getCurrencyInstance(locale);
+    }
+
+    private String formatProduct(Product product) {
+      return MessageFormat.format(resources.getString("product"), product.getName(),
+          moneyFormat.format(product.getPrice()), product.getRating().getStars(),
+          dateFormat.format(product.getBestBefore()));
+    }
+
+    private String formatReview(Review review) {
+      return MessageFormat.format(resources.getString("review"), review.getRating().getStars(),
+          review.getComments());
+    }
+
+    private String getText(String key) {
+      return resources.getString(key);
+    }
   }
 }
 
